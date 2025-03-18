dc0666033e8d4591e6ff211125e04f775d8d6c23
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-03-19 03:41:20 +0500

    341am

--- qachatbot/qa-chatbot/main.py
+++ qachatbot/qa-chatbot/main.py
@@ -1,3 +1,17 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv["GOOGLE_API_KEY"])
+                
+model = genai.GeneratorExit(model_name= "gemini-20,") # Load the model 
+
+response = model.generate_content_async("Hello, how are you?") # Generate content
+
+print(response.text) # Print the response
+
+
+print(response.text) # Print the response
