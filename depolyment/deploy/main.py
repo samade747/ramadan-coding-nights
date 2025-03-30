39ae30e1911fd9ba429b63cf00217be0ae943390
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-03-30 17:15:53 +0500

    515pm 30 03 2025

---
+++ depolyment/deploy/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.title('Hello world samad streamlit app ' )
+
+user_input = st.text_input("Enter text: ")
+
+if st.button("show text"):
+    st.write("You entered:", user_input)
+    
